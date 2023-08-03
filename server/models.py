c2b5d1b49308c5bdafdac08a44324fe93016634e
Linda Seiter <linseiter@gmail.com>
2023-08-03 09:44:57 -0400

    remove Department from initial models.py

--- server/models.py
+++ server/models.py
@@ -19,14 +19,3 @@ class Employee(db.Model):
 
     def __repr__(self):
         return f'<Employee {self.id}, {self.name}, {self.salary}>'
-
-
-class Department(db.Model):
-    __tablename__ = 'departments'
-
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String, nullable=False)
-    location = db.Column(db.String)
-
-    def __repr__(self):
-        return f'<Department {self.id}, {self.name} {self.location}>'
