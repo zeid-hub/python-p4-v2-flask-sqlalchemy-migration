7cfac3f2a53922117ba5aa9f20e326053fc0ee85
zeid <zeidhassan4@gmail.com>
2024-04-02 18:31:25 +0300

    Done

--- server/models.py
+++ server/models.py
@@ -19,3 +19,12 @@ class Employee(db.Model):
 
     def __repr__(self):
         return f'<Employee {self.id}, {self.name}, {self.salary}>'
+class Department(db.Model):
+    __tablename__ = 'departments'
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String, nullable=False)
+    address = db.Column(db.String)
+
+    def __repr__(self):
+        return f'<Department {self.id}, {self.name} {self.address}>'
